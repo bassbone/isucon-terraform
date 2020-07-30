d232420bed6eda503925b2b0f67d5ed648e30bf8
bassbone <bassbone0o_o0@yahoo.co.jp>
2020-07-30 11:40:38 +0900

    rename

--- isucon9-qualify/instances.tf
+++ isucon9-qualify/instances.tf
@@ -4,10 +4,10 @@ module "gce_bench" {
     tags = "${var.name}-bench"
     network_name = google_compute_network.default.name
     subnetwork_name = google_compute_subnetwork.default.name
-    metadata_startup_script = file("startup-script-bench.sh")
+    metadata_startup_script = file("./startup-script/startup-script-bench.sh")
     node_count = var.bench-count
     static_ip = google_compute_address.static_ip_bench
-    offset = 50
+    offset = 201
 }
 
 module "gce_web" {
@@ -16,9 +16,9 @@ module "gce_web" {
     tags = "${var.name}-web"
     network_name = google_compute_network.default.name
     subnetwork_name = google_compute_subnetwork.default.name
-    metadata_startup_script = templatefile("startup-script-web.sh", { bench-ip = module.gce_bench.network_interface[0][0].access_config.0.nat_ip })
+    metadata_startup_script = templatefile("./startup-script/startup-script-web.sh", { bench-ip = module.gce_bench.network_interface[0][0].access_config.0.nat_ip })
     node_count = var.web-count
     static_ip = google_compute_address.static_ip_web
-    offset = 100
+    offset = 101
 }
 
