39e7349dc515293d8313df77616f49ed4327d55d
bassbone <bassbone0o_o0@yahoo.co.jp>
2020-08-09 01:54:32 +0900

    modify firewalls

--- isucon9-qualify/firewalls.tf
+++ isucon9-qualify/firewalls.tf
@@ -32,3 +32,25 @@ resource "google_compute_firewall" "external" {
     target_tags = ["${var.name}-bench"]
 }
 
+resource "google_compute_firewall" "internal" {
+    name = "${var.name}-internal"
+    network = google_compute_network.default.name
+    allow {
+        protocol = "tcp"
+        ports = ["0-65535"]
+    }
+    source_ranges = ["10.0.0.0/16"]
+    target_tags = ["${var.name}-web"]
+}
+
+resource "google_compute_firewall" "netdata" {
+    name = "${var.name}-netdata"
+    network = google_compute_network.default.name
+    allow {
+        protocol = "tcp"
+        ports = ["19999"]
+    }
+    source_ranges = ["0.0.0.0/0"]
+    target_tags = ["${var.name}-web"]
+}
+
